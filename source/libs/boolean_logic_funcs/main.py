3515e4516c3018e9886d3ba749c89701189a6db5
Gleb Nechaev <nechaevgleb@inbox.ru>
2022-09-18 21:54:09 +0300

    Second commit

--- source/libs/boolean_logic_funcs/main.py
+++ source/libs/boolean_logic_funcs/main.py
@@ -1,5 +1,7 @@
 import math
 
+from pylatex import Math
+
 import pandas as pd
 import dataframe_image as dfi
 
@@ -10,7 +12,8 @@ class BooleanVector:
 
     def __init__(self, hex_vec: str):
         self.hex = hex_vec
-
+        self.ranks = 0
+    
     @property
     def bin(self):
         res = str('{0:08b}'.format(int(self.hex, 16)))
@@ -21,16 +24,19 @@ class BooleanVector:
     @property
     def bin_table(self):
         table_dict = {}
-        ranks = int(math.log2(len(self.bin)))
+        self.ranks = int(math.log2(len(self.bin)))
 
-        for rank in range(ranks - 1, -1, -1):
+        for rank in range(self.ranks - 1, -1, -1):
             table_dict['x' + str(rank)] = self._gen_bin_col(len(self.bin), 2 ** rank)
         
-        table_dict['y'] = [ bit for bit in self.bin]
+        table_dict['y'] = [bit for bit in self.bin]
         return pd.DataFrame(table_dict)
 
-    def export_table(self, filepath='table.png'):
-        dfi.export(self.bin_table, filepath, table_conversion='matplotlib')
+    def export_table_png(self):
+        dfi.export(self.bin_table, self.hex + '_table.png', table_conversion='matplotlib')
+
+    def export_table_excel(self):
+        self.bin_table.to_excel(self.hex + '_table.xlsx')
 
     def _gen_bin_col(self, vec_len, step):
         res = []
@@ -47,14 +53,61 @@ class BooleanVector:
                 curr += 1
         return res
 
-    def FDNF(self):
-        pass
+    @property
+    def sknf(self):
+        res = ''
+        array = self.bin_table.loc[self.bin_table['y'] == '1'].index.values
+        
+        for i in array:
+            res += '('
+            for rank in range(self.ranks):
+                var = 'x'+str(rank)
+                bit = self.bin_table.loc[i].at[var]
+                if int(bit) == 1:
+                    res += 'x_' + str(rank)
+                else:
+                    res += 'not x_' + str(rank)
+                res += ' or '
 
-    def FCNF(self):
-        pass
+            res = res[:-4]
+            res += ') and '
+        return res[:-5] 
 
+    @property
+    def sdnf(self):
+        res = ''
+        array = self.bin_table.loc[self.bin_table['y'] == '0'].index.values
+        
+        for i in array:
+            res += '('
+            for rank in range(self.ranks):
+                var = 'x'+str(rank)
+                bit = self.bin_table.loc[i].at[var]
+                if int(bit) == 0:
+                    res += 'x_' + str(rank)
+                else:
+                    res += 'not x_' + str(rank)
+                res += ' and '
+
+            res = res[:-5]
+            res += ') or '
+        return res[:-4] 
+
+    def export_sdnf(self):
+        res = self.sdnf.replace('or', '\\vee').replace('and', '\\wedge').replace('not', '\\overline')
+        file = open(self.hex + '_sdnf.txt', 'w')
+        file.write(res)
+        file.close()
+        return res
+    
+    def export_sknf(self):
+        res = self.sknf.replace('or', '\\vee').replace('and', '\\wedge').replace('not', '\\overline')
+        file = open(self.hex + '_sknf.txt', 'w')
+        file.write(res)
+        file.close()
+        return res
 
 
 if __name__ == '__main__':
     bool_vec = BooleanVector("1112666b")
-    print(bool_vec.export_table(filepath='1112666b_table.png'))
+    bool_vec.export_table_excel()
